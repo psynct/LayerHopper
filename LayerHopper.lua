087efeef1391915439cb00ff13386b6545e0484a
psynct <66823362+psynct@users.noreply.github.com>
2020-07-20 22:02:38 -0700

    Update version

--- LayerHopper.lua
+++ LayerHopper.lua
@@ -1,7 +1,7 @@
 LayerHopper = LibStub("AceAddon-3.0"):NewAddon("LayerHopper", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0", "AceTimer-3.0")
 LayerHopper.Dialog = LibStub("AceConfigDialog-3.0")
 LayerHopper:RegisterChatCommand("lh", "ChatCommand")
-LayerHopper.VERSION = 157
+LayerHopper.VERSION = 158
 
 local L = LibStub("AceLocale-3.0"):GetLocale("LayerHopper")
 
